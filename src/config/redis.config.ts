3917c130ac11c700cc8705af2a611dcd1243ff21
Jyotishmoy12 <93263133+Jyotishmoy12@users.noreply.github.com>
2025-08-28 19:59:35 +0530

    added processors and queues in booking service for async communication

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,12 +1,39 @@
-import IORedis from 'ioredis';
+import IORedis, { Redis } from 'ioredis';
 import Redlock from 'redlock';
 
-export const redisClient = new IORedis(process.env.REDIS_SERVER_URL || 'redis://localhost:6379');
+//export const redisClient = new IORedis(process.env.REDIS_SERVER_URL || 'redis://localhost:6379');
+
+function connectToRedis() {
+  try {
+    let connection:Redis;
+    
+    return () =>{
+        if(!connection){
+            connection = new IORedis(process.env.REDIS_SERVER_URL || 'redis://localhost:6379');;
+            return connection;
+        }
+        return connection;
+    }
+
+    
+  } catch (error) {
+    console.error("Error connecting to Redis:", error);
+    throw error;
+  }
+}
+// Singleton pattern to ensure a single Redis connection instance
+// throughout the application lifecycle
+// when we call getRedisConnectionObject it will return the connectToRedis function
+// then we call that function it will return the Redis connection instance
+// if the connection instance is not created it will create one and return it
+// if the connection instance is already created it will return the existing one
+export const getRedisConnectionObject = connectToRedis();
+
 
 export const redlock = new Redlock(
     // You should have one client for each independent redis node
     // or cluster
-    [redisClient],
+    [getRedisConnectionObject()],
     {
         // the expected clock drift; for more details
         // see http://redis.io/topics/distlock
